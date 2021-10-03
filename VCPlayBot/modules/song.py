73058a957a3c5b1af6d756816acf02b91599aeff
Aleesa <88157026+anuja2003@users.noreply.github.com>
2021-10-03 22:13:00 +0530

    Update song.py

--- VCPlayBot/modules/song.py
+++ VCPlayBot/modules/song.py
@@ -61,7 +61,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = "**🎵 Uploaded🤪 by @Musicworldanu**"
+        rep = "**😍 මෙන්න {NAME} ඔයාගෙ  Song  එක😊 , 🎵 Uploaded by 👉 @Musicworldanu 😍**"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
